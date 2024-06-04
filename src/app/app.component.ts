4e83eeeaf5d5316f004b86d77726e4170c191c45
Salman-shazz <salmanfaristp159@gmail.com>
2024-06-04 16:11:27 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'shazzway';
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductComponent } from './pages/product/product.component';
+import { SingleComponent } from './single/single.component';
+
+export const routes: Routes = [
+    {path:"",component:HomeComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'product',component:ProductComponent},
+    {path:'product/:id',component:SingleComponent}
+];
